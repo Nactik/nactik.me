f0619ce5f0b35b68516c79348e0dd11244a2f93c
Nathan Capiaux <capiauxnathan@gmail.com>
2022-11-01 19:41:13 +0100

    Added basic navbar

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,3 +1,50 @@
-export const Navbar = () => {
-  return <nav className=""></nav>;
+import { ThemeSwitcher } from '../../components/ThemeSwitcher';
+
+interface NavItem {
+    url: string;
+    label: string;
+    icon?: string;
+}
+
+const navItems: NavItem[] = [
+    {
+        url: '#about',
+        label: 'About Me',
+    },
+    {
+        url: '#projects',
+        label: 'Projects',
+    },
+    {
+        url: 'https://github.com/Nactik',
+        label: 'GitHub',
+    },
+    {
+        url: 'https://github.com/Nactik',
+        label: 'GitHub',
+    },
+    {
+        url: 'https://github.com/Nactik',
+        label: 'GitHub',
+    },
+];
+
+export const Navbar = ({ setTheme = (string: string) => {} }) => {
+    return (
+        <nav className="flex flex-row justify-between">
+            <div className="p-3 text-ctp-text">Nathan Capiaux</div>
+            <ul className="flex flex-row">
+                {navItems.map((element: NavItem) => {
+                    return (
+                        <a href={element.url} target="blank">
+                            <li className="p-3 mr-2 text-ctp-text">{element.label}</li>
+                        </a>
+                    );
+                })}
+                <li className="p-3 mr-2">
+                    <ThemeSwitcher setTheme={setTheme}></ThemeSwitcher>
+                </li>
+            </ul>
+        </nav>
+    );
 };
