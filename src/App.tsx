96fb6e8b36d0dd82b3c0d5912017a30e437bd614
Nathan Capiaux <capiauxnathan@gmail.com>
2022-11-03 00:57:07 +0100

    change global app min/max height

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './App.scss';
 import { Navbar } from './layouts/Navbar';
+import { AboutMe } from './layouts/AboutMe';
 
 function App() {
     const [theme, setTheme] = useState<string>('ctp-latte');
 
     return (
-        <div className={`App min-h-screen ${theme} bg-ctp-base font-mono`}>
+        <div className={`App min-h-screen max-h-fit ${theme} bg-ctp-base font-mono`}>
             <Navbar setTheme={setTheme}></Navbar>
+            <AboutMe></AboutMe>
         </div>
     );
 }
